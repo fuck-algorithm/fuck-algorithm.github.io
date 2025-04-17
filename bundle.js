8a7d7720bc6fe3f51b65665190d238cd75e62264
CC11001100 <CC11001100@qq.com>
2025-04-17 19:15:18 +0000

    Deploying to gh-pages from @ fuck-algorithm/fuck-algorithm.github.io@77f1ec33b2dd3841a56a491cdd4c9691e20eecbd 🚀

---
+++ bundle.js
@@ -0,0 +1,7 @@
+// 简单的JS代码
+document.addEventListener('DOMContentLoaded', function() {
+  const root = document.getElementById('root');
+  if (root) {
+    root.innerHTML = '<div style="text-align:center;padding:50px;"><h1>Fuck Algorithm</h1><p>把一天能理解的知识，缩短到一个小时</p><a href="https://github.com/fuck-algorithm">访问GitHub</a></div>';
+  }
+});
